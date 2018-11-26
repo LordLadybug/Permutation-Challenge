867545b146d68c064a3cbc5f51d804b77dcd6874
LordLadybug <34900293+LordLadybug@users.noreply.github.com>
2018-11-26 11:24:15 -0800

    Broken implementation with pointers
    Does not compile any more, as swapCharacters has issues related to type-checking that prevent it from compiling, but should handle the data in a much safer way than before, preventing any out-of-bounds errors while still pushing any changes to the string on to the original string. Decided to push changes to original string instead of making a copy of the string to manipulate as we don't have any further need to use the original string anyways.

--- PermutationChallenge/main.cpp
+++ PermutationChallenge/main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+void swapCharacters(std::string *PermutableString);
+
 int main(int argc, char* argv[])
 {
     if (argc != 2)
@@ -20,18 +22,32 @@ int main(int argc, char* argv[])
             return 1;
         }
         cout << "String: " << argv[1] << endl;
-            unsigned int stringLength = stringToPermute.length();
+        unsigned int stringLength = stringToPermute.length();
+        std::string *StringManipulator; //Pointer that will be used to swap around characters in stringToPermute
+        StringManipulator = &stringToPermute;
         for (unsigned int i=0; i<stringLength*2; i++)
         {
-          //  char& leading_char = stringToPermute.front();
-          //  char* leading_char_location -> stringToPermute.data();
-            char* leading_char;
-            leading_char = &stringToPermute.data();
+            //  char& leading_char = stringToPermute.front();
+            //  char* leading_char_location -> stringToPermute.data();
+            //const char *leading_char;
+            //leading_char = &stringToPermute.data();
 
-            stringToPermute.push_back(*leading_char);
-            stringToPermute.erase(&leading_char); //c++ does not like this; causes out-of-bounds error
+            swapCharacters(StringManipulator);
+            //stringToPermute.push_back(*leading_char);
+            //stringToPermute.erase(leading_char); //c++ does not like this; causes out-of-bounds error
             cout<< stringToPermute << endl;
         }
+        delete StringManipulator; //deleting for good measure
+        StringManipulator = nullptr;  //reassign pointer as a null pointer just after deleting
+
         return 0;
     }
+}
+
+void swapCharacters (std::string *PermutableString)
+{
+    const char *leading_char;
+    leading_char = PermutableString.data();
+    PermutableString.push_back(*leading_char);
+    PermutableString.erase(leading_char);
 }
